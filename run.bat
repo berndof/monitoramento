28c55dd3ce062acf8fd0365e3fdac097b1fc52d3
berndof <berndofb@gmail.com>
2025-12-03 08:45:03 -0300

    update run

--- run.bat
+++ run.bat
@@ -1,5 +1,8 @@
 @echo off
+:: Navega para o diretório onde o script .bat está
 cd /d %~dp0
-call .venv\Scripts\activate.bat
-python main.py
+
+:: Executa o script principal usando 'uv run'
+uv run main.py
+
 
